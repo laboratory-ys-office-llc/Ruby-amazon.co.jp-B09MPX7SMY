ac14178784fdc2b7056db561ba3ed7db6b0a52a0
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-09-03 18:14:20 +0900

    rubocop 警告対応

--- Chapter-10/test/effects_test.rb
+++ Chapter-10/test/effects_test.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require_relative '../lib/effects'
 
+# This test will fail because the Effects class is not defined.
 class EffectsTest < Minitest::Test
   def test_reverse
     assert Effects
